b760d99a1f0ddb6f4f42d806731cc89a7ed2bb7a
Angelo <gangelos630@gmail.com>
2024-06-01 17:18:27 -0500

    configured pom.xml whit sdk paypal, create DataPayment, PaypalConfig, PaypalController and PaypalService

---
+++ src/main/java/pro/angelogomez/ecommerce/backend/infrastructure/rest/PaypalController.java
@@ -0,0 +1,42 @@
+package pro.angelogomez.ecommerce.backend.infrastructure.rest;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pro.angelogomez.ecommerce.backend.domain.model.DataPayment;
+import pro.angelogomez.ecommerce.backend.infrastructure.service.PaypalService;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/payments")
+public class PaypalController {
+    private final PaypalService paypalService;
+    private final String SUCCESS_URL = "http://localhost:8085/api/v1/payments/success";
+    private final String CANCEL_URL = "http://localhost:8085/api/v1/payments/cancel";
+
+    @PostMapping
+    public String createPayment(@RequestBody DataPayment dataPayment) {
+        try {
+            Payment payment = paypalService.createPayment(
+                    Double.valueOf(dataPayment.getCurrency()),
+                    dataPayment.getCurrency(),
+                    dataPayment.getMethod(),
+                    "SALE",
+                    dataPayment.getDescription(),
+                    CANCEL_URL,
+                    SUCCESS_URL
+            );
+            for (Links links : payment.getLinks()){
+                if (links.getRel().equals("approval_url")){
+                    return links.getHref();
+                }
+            }
+        } catch (PayPalRESTException e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+}
